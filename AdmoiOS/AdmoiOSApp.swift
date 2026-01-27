512ee76afa4b54aa342b4202481551b879a9363d
“Eugene <e.cullen@student.reading.ac.uk>
2026-01-27 19:39:14 +0000

    Initial Commit

---
+++ AdmoiOS/AdmoiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdmoiOSApp.swift
+//  AdmoiOS
+//
+//  Created by Eugene Cullen on 27/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct AdmoiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
